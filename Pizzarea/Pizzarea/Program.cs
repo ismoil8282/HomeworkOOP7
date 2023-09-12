e7922a5d92e0151d77d1d6986f271d6f8d0dfaf3
ismoil8282 <137428819+ismoil8282@users.noreply.github.com>
2023-09-12 18:43:17 +0300

    1

---
+++ Pizzarea/Pizzarea/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Pizzarea
+{
+    internal class Program
+    {
+          /*  Pizzeria uchun konsol ilova tuzing.Pizzeria xodimlari uchun
+            mijozlar tomonidan keladigan buyurtmalarni raqamlashtirish 
+            kerak.TopPizza xodimlari tizimda quyidagi amallarni bajara 
+            kerak: Kassir mavjud bo'lgan pizzalardan birini tanlab pizza 
+            tayyorlash xodimlariga jo'natishi kerak(Shunchaki pizza tanlanib
+            tayyorlash processini boshlasa yetarli) va pizzani tayyorlash 
+            bosqichlari tizimda ko'rinib turishi kerak. Pizzani tayyorlashda
+            quyidagi bosqichlar mavjud: "Hamr yoyish" -> "Ingredientlar terish" 
+            -> "Duxovkada isitish" -> "Bo'laklarga bo'lish" -> "Korobkaga upakovka qilish"*/
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+
+}
